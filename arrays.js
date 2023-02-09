79301b2fa991205877ebb859476fcf4f1ac74a51
SLopezGranados <123501669+SLopezGranados@users.noreply.github.com>
2023-02-08 19:22:01 -0600

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,47 @@
+//var ListaDeCompras = [];
+//ListaDeCompras[3] = 'Tomates';
+//ListaDeCompras[1] = 'Lechuga';
+//ListaDeCompras[2] = 'Platanos';
+//ListaDeCompras[10] = 'limones';
+//ListaDeCompras[4] = 'pan';
+
+//console.log(ListaDeCompras.length);
+//var elementoDelArray = ListaDeCompras[2];
+//console.log(elementoDelArray);
+
+//var elementoDelArray = ListaDeCompras[1];
+//console.log(elementoDelArray);
+
+//var elementoDelArray = ListaDeCompras[3];
+//console.log(elementoDelArray);
+
+//var elementoDelArray = ListaDeCompras[10];
+//console.log(elementoDelArray);
+
+//var elementoDelArray = ListaDeCompras[4];
+//console.log(elementoDelArray);
+
+
+//var colores = ['amarillo', 'azul'];
+//colores.push('rojo');
+//colores.unshift('verde');
+//colores.pop();
+//colores.shift();
+//console.log(colores);
+
+//var pintores = ['Picasso', 'Velazquez', 'Van gogh', 'Dali'];
+//var incluyePintor =pintores.includes('Van gogh');
+//console.log(incluyePintor);
+
+//var numeros = [10, 6, 8, 9];
+//var cumplenCondicion = numeros.every((num) => {
+//    return num > 5;
+//});
+//console.log(cumplenCondicion);
+
+var palabra ='Henri';
+var palabraSeparada = palabra.split('');
+palabraSeparada.pop();
+palabraSeparada.push('y');
+var palabraArreglada = palabraSeparada.join('');
+console.log(palabraArreglada);
